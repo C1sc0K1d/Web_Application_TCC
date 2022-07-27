74bcfd1bd9068c075439431458475c9f3a58224c
Calil <gabrielcalil12@hotmail.com>
2022-07-27 17:34:02 -0300

    sign page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'app';
   public isWelcome = false;
+
+  constructor(private router: Router) {}
     
   ngOnInit() {
     this.isWelcome = window.location.href.includes("welcome");    
+  }
+
+  toStartPage() : void {
+    this.router.navigate(['/welcome']);
   }
   
 }
